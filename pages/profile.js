01e3367a1c6756e3500586f9895ef32bc4e67a5f
Pierre <pierre.lam91@gmail.com>
2019-09-12 20:00:09 +0200

    [ FEATURE ] Now able to log out and clear cookies

--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,6 @@
 import { getUserProfile } from '../lib/auth';
 import Layout from '../components/Layout';
+import { authInitialProps } from '../lib/auth';
 
 export default class Profile extends React.Component {
   state = {
@@ -11,9 +12,11 @@ export default class Profile extends React.Component {
   }
   render() {
     return (
-      <Layout title="Profile">
+      <Layout title="Profile" {...this.props}>
         <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
       </Layout>
     );
   }
 }
+
+Profile.getInitialProps = authInitialProps();
